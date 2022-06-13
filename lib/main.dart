d688c30628bb995bc390c495305a3647fed32a67
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-13 22:57:45 +0800

    added Provider and simple check

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:amazon_clone/common/widgets/bottom_bar.dart';
 import 'package:amazon_clone/constants/global_var.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/auth/services/auth_service.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 
@@ -12,20 +18,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Amazon Clone',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => UserProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Amazon Clone',
+        theme: ThemeData(
+          scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+          colorScheme: const ColorScheme.light(
+            primary: GlobalVariables.secondaryColor,
+          ),
+          appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(color: Colors.black),
+          ),
         ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(color: Colors.black),
-        ),
+        onGenerateRoute: generateRoute,
+        home: const FirstRoute(),
       ),
-      onGenerateRoute: generateRoute,
-      home: const AuthScreen(),
     );
+  }
+}
+
+class FirstRoute extends StatefulWidget {
+  const FirstRoute({super.key});
+
+  @override
+  State<FirstRoute> createState() => _FirstRouteState();
+}
+
+class _FirstRouteState extends State<FirstRoute> {
+  
+  final authService = AuthService();
+
+  @override
+  void initState() {
+    authService.getUserData(context: context);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final tokenValid =
+        Provider.of<UserProvider>(context, listen: false).user.token.isNotEmpty;
+    return tokenValid ? const BottomBar() : const AuthScreen();
   }
 }
