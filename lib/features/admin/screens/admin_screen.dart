dd6d3d273112a00344ca2c72a82605175009ce60
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-14 23:40:54 +0800

    added Admin Screen

---
+++ lib/features/admin/screens/admin_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({super.key});
+
+  static const routeName = '/admin';
+
+  @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
