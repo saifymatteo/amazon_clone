326175ef6ef3f65924a4540e8a30d2ec9cf9afd8
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:26:23 +0800

    finishing up CategoryDealsScreen

--- lib/features/home/screens/category_deals.screen.dart
+++ lib/features/home/screens/category_deals.screen.dart
@@ -1,4 +1,8 @@
+import 'package:amazon_clone/common/widgets/loader.dart';
 import 'package:amazon_clone/constants/global_var.dart';
+import 'package:amazon_clone/features/home/services/home_services.dart';
+import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class CategoryDealScreen extends StatefulWidget {
@@ -12,6 +16,23 @@ class CategoryDealScreen extends StatefulWidget {
 }
 
 class _CategoryDealScreenState extends State<CategoryDealScreen> {
+  final homeService = HomeServices();
+  List<Product>? _productList;
+
+  Future<void> fetchProducts() async {
+    _productList = await homeService.fetchCategoryProducts(
+      context: context,
+      category: widget.category,
+    );
+    setState(() {}); // Rebuild after complete
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProducts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,18 +51,75 @@ class _CategoryDealScreenState extends State<CategoryDealScreen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            alignment: Alignment.topLeft,
-            child: Text(
-              'Keep shopping for ${widget.category}',
-              style: const TextStyle(fontSize: 20),
+      body: _productList == null
+          ? const Loader()
+          : Column(
+              children: [
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Keep shopping for ${widget.category}',
+                    style: const TextStyle(fontSize: 20),
+                  ),
+                ),
+                SizedBox(
+                  height: 170,
+                  child: GridView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.only(left: 15),
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 1,
+                      childAspectRatio: 1.4,
+                    ),
+                    itemCount: _productList!.length,
+                    itemBuilder: (_, index) {
+                      final product = _productList![index];
+
+                      return GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(
+                            context,
+                            ProductDetailScreen.routeName,
+                            arguments: product,
+                          );
+                        },
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 130,
+                              child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.black12,
+                                    width: 0.5,
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Image.network(product.images[0]),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.topLeft,
+                              padding: const EdgeInsets.only(top: 5, right: 15),
+                              child: Text(
+                                product.name,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
