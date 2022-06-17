a65cb7173aa4c8dd898aaa97bf8bb91eca62d390
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:20:48 +0800

    added color option

--- lib/common/widgets/custom_button.dart
+++ lib/common/widgets/custom_button.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, this.text, this.onTap});
+  const CustomButton({super.key, this.text, this.onTap, this.color});
 
   final String? text;
   final VoidCallback? onTap;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
@@ -12,8 +13,14 @@ class CustomButton extends StatelessWidget {
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
         minimumSize: const Size(double.infinity, 50),
+        primary: color,
       ),
-      child: Text(text!),
+      child: Text(
+        text!,
+        style: TextStyle(
+          color: color == null ? Colors.white : Colors.black,
+        ),
+      ),
     );
   }
 }
