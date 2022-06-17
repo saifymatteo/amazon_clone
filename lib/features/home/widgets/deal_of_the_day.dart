df926a51dbe63cf22ceefa03d79d8668a99e19a8
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:24:59 +0800

    finished up Deal of The Day custom Widget

--- lib/features/home/widgets/deal_of_the_day.dart
+++ lib/features/home/widgets/deal_of_the_day.dart
@@ -1,4 +1,7 @@
-import 'package:amazon_clone/constants/global_var.dart';
+import 'package:amazon_clone/common/widgets/loader.dart';
+import 'package:amazon_clone/features/home/services/home_services.dart';
+import 'package:amazon_clone/features/product_details/screens/product_details_screen.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class DealOfTheDay extends StatefulWidget {
@@ -9,83 +12,97 @@ class DealOfTheDay extends StatefulWidget {
 }
 
 class _DealOfTheDayState extends State<DealOfTheDay> {
+  final homeService = HomeServices();
+  Product? product;
+
+  Future<void> fetchDealOfDay() async {
+    product = await homeService.fetchDealOfDay(context: context);
+    setState(() {}); // Rebuild after complete
+  }
+
+  void navigateToDetailsScreen() {
+    Navigator.pushNamed(
+      context,
+      ProductDetailScreen.routeName,
+      arguments: product,
+    );
+  }
+
+  @override
+  void initState() {
+    fetchDealOfDay();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 10, top: 15),
-          child: const Text(
-            'Deal of the day',
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-        Image.network(
-          GlobalVariables.carouselImages[0],
-          height: 235,
-          fit: BoxFit.fitHeight,
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 15),
-          child: const Text(
-            r'$999',
-            style: TextStyle(fontSize: 18),
-          ),
-        ),
-        Container(
-          alignment: Alignment.topLeft,
-          padding: const EdgeInsets.only(left: 15, top: 5, right: 40),
-          child: const Text(
-            'Laptop',
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-          ),
-        ),
-        SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network(
-                GlobalVariables.carouselImages[1],
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
-              ),
-              Image.network(
-                GlobalVariables.carouselImages[2],
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
-              ),
-              Image.network(
-                GlobalVariables.carouselImages[3],
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
-              ),
-              Image.network(
-                GlobalVariables.carouselImages[4],
-                fit: BoxFit.fitHeight,
-                width: 100,
-                height: 100,
-              ),
-            ],
-          ),
-        ),
-        Container(
-          padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
-          alignment: Alignment.topLeft,
-          child: Text(
-            'See all deals',
-            style: TextStyle(
-              color: Colors.cyan[800],
-            ),
-          ),
-        ),
-      ],
-    );
+    return product == null
+        ? const Loader()
+        : product!.name.isEmpty
+            ? const SizedBox()
+            : GestureDetector(
+                onTap: navigateToDetailsScreen,
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(left: 10, top: 15),
+                      child: const Text(
+                        'Deal of the day',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    Image.network(
+                      product!.images[0],
+                      height: 235,
+                      fit: BoxFit.fitHeight,
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding: const EdgeInsets.only(left: 15),
+                      child: Text(
+                        '\$${product!.price.toStringAsFixed(2)}',
+                        style: const TextStyle(fontSize: 18),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.topLeft,
+                      padding:
+                          const EdgeInsets.only(left: 15, top: 5, right: 40),
+                      child: Text(
+                        product!.name,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: product!.images
+                            .map(
+                              (e) => Image.network(
+                                e,
+                                fit: BoxFit.fitHeight,
+                                width: 100,
+                                height: 100,
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        'See all deals',
+                        style: TextStyle(
+                          color: Colors.cyan[800],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              );
   }
 }
