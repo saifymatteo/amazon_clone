58d577fdbff05e82a88d9f1acb45c229d66d8618
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:21:54 +0800

    added Cart custom widget

---
+++ lib/features/cart/widget/cart_product.dart
@@ -0,0 +1,168 @@
+import 'package:amazon_clone/features/cart/services/cart_services.dart';
+import 'package:amazon_clone/features/product_details/services/product_details_services.dart';
+import 'package:amazon_clone/models/product.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartProduct extends StatefulWidget {
+  const CartProduct({super.key, required this.index});
+
+  final int index;
+
+  @override
+  State<CartProduct> createState() => _CartProductState();
+}
+
+class _CartProductState extends State<CartProduct> {
+  final productDetailService = ProductDetailsService();
+  final cartService = CartServices();
+
+  void increaseQuantity(Product product) {
+    productDetailService.addToCart(context: context, product: product);
+  }
+
+  void decreaseQuantity(Product product) {
+    cartService.removeFromCart(context: context, product: product);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final productCart = context.watch<UserProvider>().user.cart[widget.index];
+
+    final product =
+        // ignore: avoid_dynamic_calls
+        Product.fromMap(productCart['product'] as Map<String, dynamic>);
+
+    // ignore: avoid_dynamic_calls
+    final quantity = productCart['quantity'];
+
+    return Column(
+      children: [
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          child: Row(
+            children: [
+              Image.network(
+                product.images[0],
+                fit: BoxFit.fitWidth,
+                height: 135,
+                width: 135,
+              ),
+              Column(
+                children: [
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      product.name,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      maxLines: 2,
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: Text(
+                      '\$${product.price.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10),
+                    child: const Text(
+                      'Eligible for FREE Shipping',
+                    ),
+                  ),
+                  Container(
+                    width: 235,
+                    padding: const EdgeInsets.only(left: 10, top: 5),
+                    child: const Text(
+                      'In Stock',
+                      style: TextStyle(
+                        color: Colors.teal,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // ignore: use_decorated_box
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black12,
+                    width: 1.5,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.black12,
+                ),
+                child: Row(
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        decreaseQuantity(product);
+                      },
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.remove,
+                          size: 18,
+                        ),
+                      ),
+                    ),
+                    DecoratedBox(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black12,
+                          width: 1.5,
+                        ),
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(0),
+                      ),
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: Text(quantity.toString()),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        increaseQuantity(product);
+                      },
+                      child: Container(
+                        width: 35,
+                        height: 32,
+                        alignment: Alignment.center,
+                        child: const Icon(
+                          Icons.add,
+                          size: 18,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
