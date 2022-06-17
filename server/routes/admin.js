ea28c85b36f1f59c6013bea252c3de65dbfb8087
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:15:32 +0800

    fix Product const name

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const admin = require('../middleware/admin')
-const Product = require('../models/product')
+const { Product } = require('../models/product')
 
 const adminRouter = express.Router()
 
