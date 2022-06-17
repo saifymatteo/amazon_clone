573a714578047566a1cc9f9e000283690ac8a57e
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-17 23:18:21 +0800

    added cart and copywith method

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -9,12 +9,27 @@ class User {
     required this.address,
     required this.type,
     required this.token,
+    required this.cart,
   });
 
   factory User.fromJson(String source) =>
       User.fromMap(json.decode(source) as Map<String, dynamic>);
 
   factory User.fromMap(Map<String, dynamic> map) {
+    // Another one to avoid headache
+    // final cartList = List<Rating>.from(
+    //   (map['ratings'] as List<dynamic>).map(
+    //     (x) => Rating.fromMap(x as Map<String, dynamic>),
+    //   ),
+    // ).cast<Rating>().toList();
+
+    // TODO(saifymatteo): Check later
+    final cartList = List<Map<String, dynamic>>.from(
+      (map['cart'] as List<dynamic>).map(
+        (x) => Map<String, dynamic>.from(x as Map),
+      ),
+    );
+
     return User(
       id: (map['_id'] ?? '') as String,
       name: (map['name'] ?? '') as String,
@@ -23,6 +38,7 @@ class User {
       address: (map['address'] ?? '') as String,
       type: (map['type'] ?? '') as String,
       token: (map['token'] ?? '') as String,
+      cart: cartList,
     );
   }
 
@@ -35,6 +51,7 @@ class User {
       'address': address,
       'type': type,
       'token': token,
+      'cart': cart,
     };
   }
 
@@ -47,4 +64,27 @@ class User {
   final String address;
   final String type;
   final String token;
+  final List<dynamic> cart;
+
+  User copyWith({
+    String? id,
+    String? name,
+    String? email,
+    String? password,
+    String? address,
+    String? type,
+    String? token,
+    List<dynamic>? cart,
+  }) {
+    return User(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      email: email ?? this.email,
+      password: password ?? this.password,
+      address: address ?? this.address,
+      type: type ?? this.type,
+      token: token ?? this.token,
+      cart: cart ?? this.cart,
+    );
+  }
 }
