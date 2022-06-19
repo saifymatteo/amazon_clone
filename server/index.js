de4a8dd657198e928cb540f0dc4732cdb9860f93
saifymatteo <muhdsaifulmashuri@gmail.com>
2022-06-19 12:23:32 +0800

    changed default port

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const productRouter = require('./routes/product.js')
 const userRouter = require('./routes/user.js')
 
 // Set default PORT number
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 // MongoDB Url Path
 const db = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.vtonx.mongodb.net/?retryWrites=true&w=majority`
